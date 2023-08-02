5bcca98fabfe15cfbda0ca1edb81081af35c7ceb
PROPHECY0102 <boonkimgan@gmail.com>
2023-08-02 22:07:29 +0800

    v0.0.1

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import githubIcon from "../public/images/github.png";
+import twitterIcon from "../public/images/twitter.png";
+
+const githubLink = document.querySelector(".github-icon");
+const twitterLink = document.querySelector(".twitter-icon");
+
+githubLink.setAttribute("src", githubIcon);
+twitterLink.setAttribute("src", twitterIcon);
